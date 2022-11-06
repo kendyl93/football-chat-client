b16ec6596ffb1acdb848515fb07cbc8e8f524f7a
Pawel Stanecki <pawel@payflow.es>
2022-11-06 18:40:23 +0100

    Added proper endpoint

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 const App: React.FC = () => {
+  const [matches, setMatches] = useState([])
+
+  useEffect(() => {
+    const getAPIdata = async () => {
+      try {
+        const response = await axios.get('http://192.168.0.195:4001/api/chatRoom/test'); // put IP instead of localhost
+        setMatches(response?.data?.data?.matches)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getAPIdata();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.test}>Fotball chat client TS</Text>
-      <Text> footer TS </Text>
-      <StatusBar style="auto" />
-    </View>
+      <FlatList
+        data={matches}
+        renderItem={({ item }: any) => <Text>{item?.homeTeam?.name} - {item?.awayTeam?.name}</Text>}
+      />
+      < StatusBar style="auto" />
+    </View >
   );
 }
 
