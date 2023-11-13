33e75e47525c924f7015a6d0f54d620acfc247fa
pawelstanecki <p.stanecki93@gmail.com>
2023-11-13 20:40:02 +0100

    Reactivate the app on the new laptop

--- hooks.ts
+++ hooks.ts
@@ -4,30 +4,33 @@ import { useEffect, useState } from "react";
 export const API_URL = `http://192.168.1.35`; // need to put ip from ifconfig
 
 export interface ChatRoom {
-    homeTeam: any;
-    awayTeam: any;
-    matchId: number;
-    status: string;
-    utcStartDate: string;
-    expireAt: Date;
-    messages: any;
+  homeTeam: any;
+  awayTeam: any;
+  matchId: number;
+  status: string;
+  utcStartDate: string;
+  expireAt: Date;
+  messages: any;
 }
 
 export const useGetAppData = () => {
-    const [matches, setMatches] = useState<ChatRoom[]>([])
+  const [matches, setMatches] = useState<ChatRoom[]>([]);
 
-    useEffect(() => {
-        const getAPIdata = async () => {
-            try {
-                const response = await axios.get(`${API_URL}:4001/api/chatRoom/redisChatRooms`); // put IP instead of localhost
-                setMatches(response?.data?.data?.matches);
-            } catch (error) {
-                console.error(error)
-            }
-        }
+  useEffect(() => {
+    const getAPIdata = async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}:4001/api/chatRoom/redisChatRooms`
+        ); // put IP instead of localhost
 
-        getAPIdata();
-    }, [])
+        setMatches(response?.data?.data?.matches);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    return matches
-}
+    getAPIdata();
+  }, []);
+
+  return matches;
+};
--- utils/socket.ts
+++ utils/socket.ts
@@ -2,15 +2,14 @@ import io from "socket.io-client";
 
 const SOCKET_URL = `http://192.168.1.35:3025`;
 
-const socket = io(SOCKET_URL, { transports: ['websocket'] });
+const socket = io(SOCKET_URL, { transports: ["websocket"] });
 
-socket.on('connect', () => {
-    console.log('⚡️[socket] Connected to server');
+socket.on("connect", () => {
+  console.log("⚡️[socket] Connected to server");
 });
 
-socket.on('disconnect', () => {
-    console.log('⚡️[socket] Disconnected from server');
+socket.on("disconnect", () => {
+  console.log("⚡️[socket] Disconnected from server");
 });
-
 
 export default socket;
